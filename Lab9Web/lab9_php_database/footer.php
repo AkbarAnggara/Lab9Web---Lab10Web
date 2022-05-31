488c303c59d52bb816ed3d006dd2c71fefd8d290
AkbarAnggara <akbarxelion@gmail.com>
2022-05-31 07:55:20 +0700

    Update Folder

---
+++ Lab9Web/lab9_php_database/footer.php
@@ -0,0 +1,10 @@
+<div class="row">
+        <div class="col col-sm-12">
+            <div class="font-weight-bold bg-primary mb-1">
+                <div class="card-body text-center">
+                    Bangkit Akbar Anggara - 312010148 - TI.20.B.1 <br>
+                    &copy; 2022 - Universitas Pelita Bangsa - Teknik Informatika
+                </div>
+            </div>
+        </div>
+    </div>
